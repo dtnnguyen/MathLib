650a47482d4c638e374eadffb0b59514b12e1637
dtnnguyen <28605254+dtnnguyen@users.noreply.github.com>
2018-07-29 23:22:13 -0400

    fix google test

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,18 @@
 
 #include "gtest/gtest.h"
 #include <iostream>
+#include "LinearInterpolate.h"
+
+TEST(LinearInterpolate, FromZero) {
+	auto result = linearInterpolate(0.0,0.0,0.0,0.0,0.0);
+	EXPECT_EQ(result, 0.0);
+}
+
+TEST(LinearInterpolate, From100) {
+	auto result = linearInterpolate(10.0,100.0,0.0,0.0,0.0);
+	EXPECT_NE(result, 0.0);
+}
+
 
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
